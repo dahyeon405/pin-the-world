86479d3aedb284b82c2207d84d36cd07d1ff4f6e
dahyeon405 <dahyeon405@naver.com>
2023-07-06 21:19:07 +0900

    update remove loading indicator

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,32 +2,10 @@ import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 import { CitiesBoard } from "@/components/CitiesBoard";
 import { EarthCanvas } from "@/components/EarthCanvas";
-import { useEffect, useState } from "react";
-import { LoadingIndicator } from "@/components/LoadingIndicator/LoadingIndicator";
-import { useDisableScroll } from "@/hooks";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const { enableScroll, disableScroll } = useDisableScroll();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    if (!isLoading) enableScroll();
-    else disableScroll();
-  }, [isLoading]);
-
   return (
     <>
-      {isLoading && (
-        <LoadingIndicatorContainer>
-          <LoadingIndicator />
-        </LoadingIndicatorContainer>
-      )}
       <HomeLayout>
         <HomeTitle>Pin the World</HomeTitle>
         <CitiesBoardContainer>
@@ -45,16 +23,6 @@ export default function Home() {
     </>
   );
 }
-
-const LoadingIndicatorContainer = styled.h1`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  z-index: 5;
-`;
 
 const HomeTitle = styled.h1`
   font-size: 128px;
