5bd49670a558970b27ef029c6a44aab29e05838c
dahyeon405 <dahyeon405@naver.com>
2023-07-08 22:01:58 +0900

    add ResizedImage component

---
+++ src/components/ResizedImage/index.tsx
@@ -0,0 +1,23 @@
+export const ResizedImage = ({
+  imageName,
+  style,
+}: {
+  imageName: string;
+  style?: any;
+}) => {
+  const bucket_url = "https://pin-the-world.s3.ap-northeast-2.amazonaws.com/";
+
+  const original_url = bucket_url + imageName;
+
+  const ext = imageName
+    .split(".")
+    [imageName.split(".").length - 1].toLowerCase();
+  const filename = imageName.slice(0, -(ext.length + 1));
+
+  const image_1x = bucket_url + "resized/" + filename + "%26400";
+  const image_2x = bucket_url + "resized/" + filename + "%26960";
+
+  const srcSet = image_1x + " 1x," + image_2x + " 2x";
+
+  return <img srcSet={srcSet} src={image_2x} style={style} />;
+};
