3f73e94ccce5b5ff89fd02f4b4f7d381f9be6f69
dahyeon405 <dahyeon405@naver.com>
2023-07-18 22:25:01 +0900

    fix coast line drawing error

--- src/components/EarthCanvas/Earth/Coastline/singleCoastLine.tsx
+++ src/components/EarthCanvas/Earth/Coastline/singleCoastLine.tsx
@@ -12,33 +12,36 @@ export default function SingleCoastLine({
   radius,
   material,
 }: SingleCoastLineProps) {
-  const vertex = (location: [number, number], radius: number) => {
-    const vector = convertCoordinateToVector(location, radius);
-
-    return new THREE.Vector3(...vector);
-  };
-
-  const wireframe = (coordinates: Array<[number, number]>, radius: number) => {
+  const getBufferGeometry = (
+    coordinates: Array<[number, number]>,
+    radius: number
+  ) => {
     const geometry = new THREE.BufferGeometry();
-    const points = [] as Array<THREE.Vector3>;
+    const points: Array<THREE.Vector3> = [];
 
-    for (let i = 0; i < coordinates.length; i++) {
-      const vector3 = vertex(coordinates[i], radius);
+    coordinates.forEach((item) => {
+      const vector3 = convertCoordinateToVector3(item, radius);
       points.push(vector3);
-    }
+    });
 
     geometry.setFromPoints(points);
 
     return geometry;
   };
 
-  return (
-    <line>
-      <edgesGeometry
-        attach="geometry"
-        args={[wireframe(coordinates, radius)]}
-      />
-      <lineBasicMaterial {...material} />
-    </line>
-  );
+  const convertCoordinateToVector3 = (
+    location: [number, number],
+    radius: number
+  ) => {
+    const vector = convertCoordinateToVector(location, radius);
+
+    return new THREE.Vector3(...vector);
+  };
+
+  const _geometry = getBufferGeometry(coordinates, radius);
+  const _material = new THREE.LineBasicMaterial(material);
+
+  const line = new THREE.Line(_geometry, _material);
+
+  return <primitive object={line} position={[0, 0, 0]} />;
 }
