dea8f95489487df07a18981656df241681b41f97
dahyeon405 <dahyeon405@naver.com>
2023-07-19 00:02:50 +0900

    refactor pins

--- src/components/EarthCanvas/Earth/Pins/index.tsx
+++ src/components/EarthCanvas/Earth/Pins/index.tsx
@@ -1,18 +1,28 @@
 import { cityCoordinates } from "@/constants/cityCoordinates";
-import { convertCoordinateToVector } from "../../../../utils/convertCoordinateToVector";
-import SinglePin from "./singlePin";
+import { convertCoordinateToVector } from "@/utils/convertCoordinateToVector";
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+import { useLoader } from "@react-three/fiber";
 
 export default function Pins({ radius }: { radius: number }) {
+  const pin = useLoader(OBJLoader, "./starPin.obj");
+
+  pin.traverse((obj: any) => {
+    if (obj.isMesh) {
+      obj.material.color.set(0x000000);
+    }
+  });
+
   return (
     <group>
       {Object.values(cityCoordinates).map((coordinate, index) => {
         return (
-          <SinglePin
+          <primitive
             position={convertCoordinateToVector(
               coordinate as [number, number],
               radius
             )}
             scale={0.08}
+            object={pin.clone()}
             key={index}
           />
         );
