dd2ab1f919ba769658b1f4da8145848a2a783bf7
dahyeon405 <dahyeon405@naver.com>
2023-07-18 20:34:15 +0900

    fix obj file load

--- src/components/EarthCanvas/Earth/Pins/singlePin.tsx
+++ src/components/EarthCanvas/Earth/Pins/singlePin.tsx
@@ -1,5 +1,6 @@
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { useLoader } from "@react-three/fiber";
+import StarPinObj from "@/assets/starPin.obj";
 
 interface SinglePinProps {
   position: number[];
@@ -7,7 +8,7 @@ interface SinglePinProps {
 }
 
 export default function SinglePin({ position, scale }: SinglePinProps) {
-  const pin = useLoader(OBJLoader, "./starPin.obj");
+  const pin = useLoader(OBJLoader, StarPinObj);
 
   pin.traverse((obj: any) => {
     if (obj.isMesh) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import ObjFileImport from "unplugin-obj/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ObjFileImport()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
