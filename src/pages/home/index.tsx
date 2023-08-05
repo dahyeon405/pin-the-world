41d1a91c28edd158f1fdb345d2cf59fe3896979b
dahyeon405 <dahyeon405@naver.com>
2023-08-05 20:14:56 +0900

    fix can't touch canvas error

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,20 +2,12 @@ import { Canvas } from "@react-three/fiber";
 import { EarthCanvas } from "@/components/EarthCanvas";
 import { CityPolaroidBoard } from "@/components/Polaroid/CityPolaroidBoard";
 import { Box, Heading } from "@chakra-ui/react";
-import { useCallback } from "react";
 
 export default function Home() {
   // react-three/drei 라이브러리에서,
   // OrbitControls 사용 시 touch action = none 으로 설정되는 오류가 있음 (터치 불가능)
   // 이슈 링크: https://github.com/pmndrs/drei/issues/1233
-  const canvasContainerRef = useCallback((node: HTMLElement) => {
-    if (node !== null && node.children[0]) {
-      const target = node.children[0] as HTMLDivElement;
-      setTimeout(() => {
-        target.style.touchAction = "auto";
-      }, 1000);
-    }
-  }, []);
+  // <Canvas> 컴포넌트 위에 <Box> 컴포넌트를 덮는 것으로 해결해놓음
 
   return (
     <>
@@ -27,20 +19,14 @@ export default function Home() {
         <Heading as="h1" size="4xl" pl="24px" fontWeight="regular">
           Pin the World
         </Heading>
-        <Box
-          pos="fixed"
-          top="32"
-          w="100vw"
-          h="100vw"
-          ref={canvasContainerRef as any}
-        >
+        <Box pos="fixed" top="32" w="100vw" h="100vw">
           <Canvas
             camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 4] }}
-            ref={canvasContainerRef as any}
           >
             <pointLight position={[0, 0, 3]} />
             <EarthCanvas />
           </Canvas>
+          <Box pos="fixed" top="32" w="100vw" h="100vw"></Box>
         </Box>
         <CityPolaroidBoard />
       </Box>
