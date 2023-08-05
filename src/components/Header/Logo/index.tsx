f869ce4008205160e05b475145d7ab51c6f536ee
dahyeon405 <dahyeon405@naver.com>
2023-08-05 22:22:38 +0900

    fix logo not shown error

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -1,8 +1,7 @@
 import logo from "@/assets/logo.svg";
-import { Image } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Image src={logo} alt="logo" display="inline-block" height="100%"></Image>
+    <img src={logo} alt="logo" style={{height: '100%'}}/>
   );
 }
