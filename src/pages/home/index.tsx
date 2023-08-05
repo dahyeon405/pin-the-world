f682d04eec37b1329859f90f5ac7fa5756a17fed
dahyeon405 <dahyeon405@naver.com>
2023-08-05 21:40:52 +0900

    update home background color

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Box
         pt="16"
         h="8500px"
-        bgGradient="linear(to-b, gray.50 3%, #f57ac0 30%, #e0edb2 40%, #68cbc0 63%, #fb97fb 81%, #fba397 91%)"
+        bgGradient="linear(to-b, white 3%, #f57ac0 30%, #e0edb2 40%, #68cbc0 63%, #fb97fb 81%, #fba397 91%)"
       >
         <Heading as="h1" size="4xl" pl="24px" fontWeight="regular">
           Pin the World
