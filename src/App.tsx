8ec24b821c40b465155e216ec99e527dd7296450
dahyeon405 <dahyeon405@naver.com>
2023-08-14 19:28:45 +0900

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import { Outlet } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.ts";
 import { ScrollToTop } from "@/components/ScrollToTop";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <ScrollToTop />
-      <Outlet />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ScrollToTop />
+        <Outlet />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
