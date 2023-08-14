00aeb9547fe9b342de9b619d1d96d54a5e16ef04
dahyeon405 <dahyeon405@naver.com>
2023-08-14 18:44:36 +0900

    add scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.ts";
+import { ScrollToTop } from "@/components/ScrollToTop";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <ScrollToTop />
       <Outlet />
     </ChakraProvider>
   );
