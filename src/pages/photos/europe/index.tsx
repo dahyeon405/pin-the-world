c7d9bd88ee8a2750a46bb3bda08bc9c5eef3b289
dahyeon405 <dahyeon405@naver.com>
2023-08-14 23:57:25 +0900

    update europe page

--- src/pages/photos/europe/index.tsx
+++ src/pages/photos/europe/index.tsx
@@ -1,3 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
 export default function EuropePage() {
-  return <div>Europe</div>;
+  return (
+    <>
+      <Box px="6" py="16">
+        <div>페이지 준비 중입니다..</div>
+      </Box>
+    </>
+  );
 }
