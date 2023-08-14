8fec542f362d46bb32ad8ca72231e1ad87c553e8
dahyeon405 <dahyeon405@naver.com>
2023-08-14 18:46:18 +0900

    update about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -10,23 +10,24 @@ export default function AboutPage() {
         About
       </Heading>
 
-      <Text fontSize="sm" mt="8">
-        음악과 여행과 사진을 사랑하는 <br /> 낭만파 개발자가 만든 페이지입니다
-        😇 <br />
-        <br />
-        공간의 자유가 허락하는 곳에서 <br />
-        일과 삶의 균형을 찾기를 꿈꾸고 있습니다 🤸‍♀️ <br />
-        <br />
-        (업데이트를 안 한 지 오래됐지만) <br />
-        음악 유튜브도 운영 중이에요!
+      <Heading as="h2" fontSize="lg" fontWeight="500" mt="8">
+        About me
+      </Heading>
+      <Text fontSize="sm" mt="2">
+        A frontend developer <br />
+        who loves to travel and take photos
       </Text>
+
+      <Heading as="h2" fontSize="lg" fontWeight="500" mt="8">
+        Youtube channel
+      </Heading>
 
       <Link href={"https://www.youtube.com/@twntytw22"}>
         <Flex
           border="1px"
           borderColor="gray.200"
           borderRadius="16px"
-          mt="8"
+          mt="2"
           px="6"
           py="3"
         >
@@ -46,20 +47,20 @@ export default function AboutPage() {
             </Text>
           </Flex>
         </Flex>
-        <Text
-          fontSize="xs"
-          mt="2"
-          align="center"
-          color="gray.600"
-          fontStyle="italic"
-        >
-          클릭해서 이동하기
-        </Text>
+        {/*<Text*/}
+        {/*  fontSize="xs"*/}
+        {/*  mt="2"*/}
+        {/*  align="center"*/}
+        {/*  color="gray.600"*/}
+        {/*  fontStyle="italic"*/}
+        {/*>*/}
+        {/*  클릭해서 이동하기*/}
+        {/*</Text>*/}
       </Link>
-      <Text fontSize="sm" mt="12">
-        사진이 마음에 드셨다면 <br />
-        인스타그램에도 놀러오세요 🤗
-      </Text>
+
+      <Heading as="h2" fontSize="lg" fontWeight="500" mt="8">
+        Instagram
+      </Heading>
 
       <Flex mt="3">
         <img src={instagramIcon} width={24} alt="instagram icon" />
@@ -74,22 +75,10 @@ export default function AboutPage() {
       </Flex>
 
       <Text fontSize="sm" my="12">
-        방문해주셔서 감사합니다. <br />
-        좋은 하루 되세요 🍀
+        Thank you for visiting my website 🤗
       </Text>
 
-      <ResizedImage imageName={"000071560034.jpg"} />
-      <Text
-        fontSize="xs"
-        mt="2"
-        align="center"
-        color="gray.600"
-        fontStyle="italic"
-      >
-        마무리는 울창한 2023년 여름의 연남동 사진으로
-      </Text>
-
-      <Text fontSize="sm" mt="12" mb="2" fontFamily="Clash Display">
+      <Text fontSize="sm" mt="24" fontFamily="Clash Display">
         2023.08.05 dahyeon
       </Text>
     </Box>
