240bcd55a5a838d7b0f1c971316d8032ca0710fd
dahyeon405 <dahyeon405@naver.com>
2023-11-01 23:09:48 +0900

    this commit is for workflow test

--- frontend/src/pages/photos/europe/index.tsx
+++ frontend/src/pages/photos/europe/index.tsx
@@ -2,6 +2,9 @@ import { PaginatedPhoto } from '@/components/Page/PaginatedPhoto'
 
 export default function EuropePage() {
   return (
-    <PaginatedPhoto params={{ travel: 'exchange' }} title={`2019 Europe`} />
+    <PaginatedPhoto
+      params={{ travel: 'exchange' }}
+      title={`2019, \n Exchange`}
+    />
   )
 }
