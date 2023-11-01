90c4ffcfb7c3f4884bb3f187ee2ac0332c5297a2
dahyeon405 <dahyeon405@naver.com>
2023-11-01 15:11:05 +0900

    update eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,30 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript",
-        "plugin:prettier/recommended",
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/comma-dangle": "off",
-        "react/no-unknown-property": "off",
-        "react/react-in-jsx-scope": "off",
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './frontend/tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'react/no-unknown-property': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
 }
